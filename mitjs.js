e1afdfd184d718be39f7933c831508c7f79024af
Emil Thomsen <emil@Emils-MacBook-Pro.local>
2019-02-21 14:27:02 +0100

    Færdigt

--- mitjs.js
+++ mitjs.js
@@ -36,11 +36,28 @@ function aktiverMultimedier(elementIdListe, AVIdListe) {
 
 
 
+
+
+document.getElementById("tilFelt2").addEventListener("click", function () {
+
+    document.getElementById("felt2").scrollIntoView(true);
+
+});
+
+document.getElementById("tilFelt3").addEventListener("click", function () {
+
+    document.getElementById("felt3").scrollIntoView(true);
+
+});
+
+
+
 //hovedprogramdel
 
+
 //1
-var elementIdListe = ["paris", "istanbul", "firenze", "video"];
-var AVIdListe = ["bouncy", "walker", "funky", "video"];
+var elementIdListe = ["felt1", "felt2", "felt3", "video"];
+var AVIdListe = ["sang1", "sang2", "sang3", "video"];
 
 //2
 window.addEventListener("scroll", function () {
